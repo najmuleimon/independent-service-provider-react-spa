457057661e953ad3d4e6ac2be48c3b09261ceac1
najmuleimon <najmul.eimon@gmail.com>
2022-04-16 23:48:20 +0600

    home about section added

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import about from '../../../images/about.jpg';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            this is about
+        <div className='about'>
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md-6">
+                        <h2>What makes <span>Muscle Magic</span> special?</h2>
+                        <p>I m a personal fitness trainer. Are u a person, who becomes fatty ,bulky and then now your career is on the line. Get back in shape! My Fitness training can help u look younger, get fit, and get back in shape. My state of the art gyms training provide you with a great guideline to work out in, whether you are there to burn off some calories or are training for something more specific. So what are you waiting for Feel free to call me? I am here to help you!</p>
+                        <h4>Great Fitness Training</h4>
+                        <p>Train with the best expert in bodybuilding field.</p>
+
+                        <h4>Yoga and meditation</h4>
+                        <p>Train with yoga and meditation also.</p>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="image">
+                            <img src={about} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
