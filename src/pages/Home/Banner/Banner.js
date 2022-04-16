edbf3f2eb6e95b3a9f9e67c96692e98e04d4e2df
najmuleimon <najmul.eimon@gmail.com>
2022-04-16 23:11:05 +0600

    banner added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            this is banner
+        <div className='banner'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 offset-lg-3 text-center">
+                        <h3>Best Fitness Trainer</h3>
+                        <h1>Muscle Magic</h1>
+                        <p>You have the best gym trainer near you. You can get all types of training to get fit and be strong with Muscle Magic.</p>
+                        <Link to="/about-me" className='about-btn'>About Me</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
