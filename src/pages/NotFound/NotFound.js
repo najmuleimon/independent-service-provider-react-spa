04a375d09ca1dc810bcad95767a35ee9c803371b
najmuleimon <najmul.eimon@gmail.com>
2022-04-17 22:44:01 +0600

    not found 404 page added

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            this is 404
+        <div className='not-found'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 offset-lg-2 text-center">
+                        <h3>404</h3>
+                        <h4>Not Found</h4>
+                        <p>The page you looking for is not found!!</p>
+                        <button onClick={() => navigate('/')}>Go back Home</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
