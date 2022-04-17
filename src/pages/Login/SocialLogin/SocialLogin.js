cdae8a97cfd25e8550edf80368d4049f1646ce13
najmuleimon <najmul.eimon@gmail.com>
2022-04-17 21:00:27 +0600

    google authentication added

--- src/pages/Login/SocialLogin/SocialLogin.js
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import {FaGoogle} from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
+import './SocialLogin.css';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, googleError] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(user){
+            navigate('/');
+            console.log(user);
+        }
+    }, [user]);
+    
+    if(loading){
+        return <Loading/>
+    }
     return (
-        <div>
-            this is social login
+        <div className='social-btn'>
+            {googleError && <p className='error-msg'>{googleError.message}</p>}
+            <button type='button' onClick={() => signInWithGoogle()}>
+                <FaGoogle className='google-icon'/>
+                Continue with Google
+            </button>
         </div>
     );
 };
